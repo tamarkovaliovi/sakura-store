2cefe705a292be1e1d1d38fb1806d87d91b89787
tamarkovaliovi <20222013305@std.nisantasi.edu.tr>
2026-02-23 20:32:23 +0300

    feat: implement category update and delete functions closes #12

--- src/router/index.js
+++ src/router/index.js
@@ -6,55 +6,60 @@ import RegisterView from '../views/RegisterView.vue';
 import ProfileView from '../views/ProfileView.vue';
 import CartView from '../views/CartView.vue';
 import ProductDetailsView from '../views/ProductDetailsView.vue';
-import AddProductView from '../views/EditProductView.vue';
 import EditProductView from '../views/EditProductView.vue';
+import CategoriesView from '../components/Categories.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/Allproducts',
+    name: 'Allproducts',
+    component: AllProductsView
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: RegisterView
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: ProfileView
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: CartView
+  },
+  {
+    path: '/products/:id',
+    name: 'product-details',
+    component: ProductDetailsView
+  },
+  {
+    path: '/edit-product/:id?', 
+    name: 'EditProduct',
+    component: EditProductView
+  },
+  {
+    path: '/kategoriler',
+    name: 'Categories',
+    component: CategoriesView 
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/Allproducts',
-      name: 'Allproducts',
-      component: AllProductsView
-    },
-    
-    {
-      path: '/login',
-      name: 'login',
-      component: LoginView
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: RegisterView
-    },
-    {
-      path: '/profile',
-      name: 'profile',
-      component: ProfileView
-    },
-    {
-      path: '/cart',
-      name: 'cart',
-      component: CartView
-    },
-    {
-      path: '/products/:id',
-      name: 'product-details',
-      component: ProductDetailsView
-    },
-    
-   {
-  path: '/edit-product/:id?', 
-  name: 'EditProduct',
-  component: EditProductView
-}
-  ]
+  routes
 });
+
 export default router;
- 
